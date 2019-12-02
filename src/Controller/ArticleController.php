40674a806266220225a6d2a1322cf2799f8f27c6
katin-dev <katin.dev@gmail.com>
2019-12-03 00:00:11 +0300

    POST /api/articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Doctrine\ORM\EntityManager;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/articles", name="articles")
+     * @Rest\Get("/articles", name="articles")
      */
     public function articles()
     {
@@ -23,5 +26,28 @@ class ArticleController extends AbstractController
         ];
 
         return View::create($articles);
+    }
+
+    /**
+     * @Rest\Post("/articles", name="articles_create")
+     * @param Request $request
+     * @param EntityManager $em
+     * @param SerializerInterface $serializer
+     * @return View
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function create(Request $request, SerializerInterface $serializer)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $r = $em->getRepository(Article::class);
+        $a = new Article();
+        $a->setName($request->get('name'));
+        $a->setContent($request->get('content'));
+
+        $em->persist($a);
+        $em->flush();
+
+        return View::create($a);
     }
 }
